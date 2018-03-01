9b56bce571968347d1d9067211925c4bb168599d
Dorisfeng <31895359+Dorisfeng@users.noreply.github.com>
2018-03-01 21:05:10 +0800

    sb_shoppingCart
    sb_shoppingCart

--- data/cart/addCart.php
+++ data/cart/addCart.php
@@ -6,13 +6,13 @@ session_start();
 $user_id=$_SESSION['uid'];
 //$uid=2;
 if($user_id&&$num&&$dish_id){
-	$sql="select * from sb_shoppingcart where user_id=$user_id and did=$dish_id";
+	$sql="select * from sb_shoppingCart where user_id=$user_id and did=$dish_id";
 	$result=mysqli_query($conn,$sql);
 	var_dump($result);
 	if(!mysqli_fetch_row($result)){
-		$sql="insert into sb_shoppingcart values(null,$user_id,$dish_id,$num,0)";
+		$sql="insert into sb_shoppingCart values(null,$user_id,$dish_id,$num,0)";
 	}else{
-		$sql="update sb_shoppingcart set num=num+$num where user_id=$user_id and did=$dish_id";
+		$sql="update sb_shoppingCart set num=num+$num where user_id=$user_id and did=$dish_id";
 	}
 	mysqli_query($conn,$sql);
 }
