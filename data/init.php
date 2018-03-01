5236310e4220d89831df16f433f4eb249c131402
Dorisfeng <31895359+Dorisfeng@users.noreply.github.com>
2018-03-01 21:04:11 +0800

    连接
    新浪云服务器

--- data/init.php
+++ data/init.php
@@ -1,6 +1,7 @@
 <?php
 header('Content-Type:application/json;charset=utf-8');
-$conn= mysqli_connect("127.0.0.1","root","","sb",3306);
+// $conn= mysqli_connect("127.0.0.1","root","","sb",3306);
+$conn= mysqli_connect("w.rdc.sae.sina.com.cn","zl2xylox03","wjhl4ljjk2l3h53k253li10j3wkxw4zh0li3xm0h","app_spicybite",3306);
 $sql="SET NAMES UTF8";
 mysqli_query($conn,$sql);
 ?>
